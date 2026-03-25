88d85fa9331953244fffc6bfba342407f58e78fd
AngriestBird <funnyman9098@gmail.com>
2026-03-24 20:50:16 -0400

    update build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+echo "INFO: Building arma-reforger-test image..."
+
 docker build -t arma-reforger-test .
+
+echo "INFO: arma-reforger-test is built. Please update your docker compose. "
